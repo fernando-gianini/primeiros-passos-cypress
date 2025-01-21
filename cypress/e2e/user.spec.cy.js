7edae32047922aba1503e316d04bd4d80614912e
Fernando Gianini <fernando.gianini.pro@gmail.com>
2025-01-21 09:36:04 -0300

    added pageObject

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,34 +1,35 @@
 import userData from '../fixtures/users/userData.json'
+import loginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import menuPage from '../pages/menuPage.js'
+
+const LoginPage = new loginPage ()
+const dashboardPage = new DashboardPage()
+const MenuPage = new menuPage()
 
 describe('Orange HRM Test', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentionAlert: "[role='alert']",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     genericDateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
+    genericSelectorButton: "[tabindex='0']",
     genericGenderFiel: ".oxd-radio-wrapper",
     genericButtonSubmitField: "[type='submit']",
+    selectNationality: ":nth-child(27) > span",
+    selectMaritalStatus: ":nth-child(3) > span",
+
   }
 
   it.only('User Info Update - success', () => {
-
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click('')
+    LoginPage.accessLoginPage()
+    LoginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+    dashboardPage.checkDashBoardPage()
+    MenuPage.accessMyInfo()
     cy.get(selectorsList.firstNameField).clear('').type ('Fernando')
     cy.get(selectorsList.middleNameField).clear('').type('Cavalheiro')
     cy.get(selectorsList.lastNameField).clear('').type('Gianini')
@@ -36,9 +37,15 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.genericField).eq(4).clear('').type('otherId-4321')
     cy.get(selectorsList.genericField).eq(5).clear('').type('driverLicente-1234')
     cy.get(selectorsList.genericDateField).eq(0).clear('').type('2025-16-12')
-    cy.get(selectorsList.dateCloseButton).click('')
-    cy.get(selectorsList.genericGenderFiel).eq(0).click('')
-    cy.get(selectorsList.genericButtonSubmitField).eq(0).click('')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericSelectorButton).eq(0).click()
+    cy.get(selectorsList.selectNationality).click()
+    cy.get(selectorsList.genericSelectorButton).eq(1).click()
+    cy.get(selectorsList.selectMaritalStatus).click()
+    cy.get(selectorsList.genericDateField).eq(1).clear('').type('1991-16-12')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericGenderFiel).eq(0).click()
+    cy.get(selectorsList.genericButtonSubmitField).eq(0).click({ force: true })
     cy.get('body').should('contain', 'Successfully Updated')
   })
 
@@ -56,7 +63,7 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentionAlert)    
-  })
+ })
 
 
-})
+ })
